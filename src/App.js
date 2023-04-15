47592a3d5a5c136d05eb4aff8ebf370cebc0daa6
gotan-dancer <eugene.kalugin@gmail.com>
2023-04-15 21:18:17 +0500

    01 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    return(
+        <div>
+            <News>
+                <Head1>
+                    <Topic href="link1" text="Сейчас в СМИ"></Topic>
+                    <Topic href="link2" text="в Германии"></Topic>
+                    <Topic href="link3" text="Рекомендуем"></Topic>
+                    <Datetime date="" day="" time=""></Datetime>
+                </Head1>
+                <NewsLinks>
+                    <News1 icon="" text="" link=""></News1>
+                    <News1 icon="" text="" link=""></News1>
+                    <News1 icon="" text="" link=""></News1>
+                    <News1 icon="" text="" link=""></News1>
+                    <News1 icon="" text="" link=""></News1>
+                </NewsLinks>
+                <Currencies>
+                    <Currency1 name="" value="" add=""></Currency1>
+                    <Currency1 name="" value="" add=""></Currency1>
+                    <Currency1 name="" value="" add=""></Currency1>
+                </Currencies>
+            </News>
+            <Errors>
+                <BigIcon></BigIcon>
+                <Work></Work>
+                <Description></Description>
+            </Errors>
+            <Ya>
+                <BigText></BigText>
+                <Links>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <Link1 text="" link=""></Link1>
+                    <LinkOther text="" link=""></LinkOther>
+                </Links>
+                <String></String>
+                <Floor>
+                    <Text></Text>
+                    <Link></Link>
+                </Floor>
+            </Ya>
+            <Banner></Banner>
+            <FloorBig>
+                <Weather>
+                    <Icon></Icon>
+                    <Temperature></Temperature>
+                    <Comment></Comment>
+                </Weather>
+                <Often>
+                    <Often1 name="" comment=""></Often1>
+                    <Often1 name="" comment=""></Often1>
+                    <Often1 name="" comment=""></Often1>
+                </Often>
+                <Maps></Maps>
+                <Tele></Tele>
+                    <Program name="" channel=""></Program>
+                    <Program name="" channel=""></Program>
+                    <Program name="" channel=""></Program>
+                <Efir>
+                    <ProgramNow name="" channel=""></ProgramNow>
+                    <ProgramNow name="" channel=""></ProgramNow>
+                    <ProgramNow name="" channel=""></ProgramNow>
+                </Efir>
+            </FloorBig>
+        </div>
+    );
 }
 
 export default App;
